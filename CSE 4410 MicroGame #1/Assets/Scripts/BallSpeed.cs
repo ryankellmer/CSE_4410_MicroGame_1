a0b6b062c3a2b4aae91e7bdd53f6ef7d789ec4a6
ryankellmer <61802954+ryankellmer@users.noreply.github.com>
2021-02-01 14:18:31 -0800

    Upload 1
    Added Paddles, Enviorment, Ball, and collisions to all the components.

---
+++ CSE 4410 MicroGame #1/Assets/Scripts/BallSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeed : MonoBehaviour
+{
+    public float ballSpeed;
+    public Rigidbody2D rb_ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Begin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Begin()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb_ball.velocity = new Vector2(ballSpeed * x, ballSpeed * y);
+    }
+}
---
+++ CSE 4410 MicroGame #1/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public bool isPaddle1;
+    public float paddleSpeed;
+    public Rigidbody2D rb_paddle;
+    private float move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPaddle1)
+        {
+            move = Input.GetAxisRaw("Vertical1");
+        }
+        else
+        {
+            move = Input.GetAxisRaw("Vertical2");
+        }
+
+        rb_paddle.velocity = new Vector2(rb_paddle.velocity.x, move * paddleSpeed);
+    }
+}
